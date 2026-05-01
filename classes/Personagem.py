cb7872707725f23de44de6d53af8d925284f826a
blesstherodrigo <rodrigo_ade@icloud.com>
2026-04-30 21:14:48 -0300

    classe inimigo

--- classes/Personagem.py
+++
@@ -1,6 +0,0 @@
-class Personagem:
-    def __init__(self, nome, vida, defesa, velocidade):
-        self.nome = nome
-        self.vida = vida
-        self.defesa = defesa
-        self.velocidade  = velocidade
---
+++ classes/jogador.py
@@ -0,0 +1,7 @@
+class Personagem:
+    def __init__(self, nome, vida, defesa, velocidade, danobase):
+        self.nome = nome
+        self.vida = vida
+        self.defesa = defesa
+        self.velocidade  = velocidade
+        self.danobase = danobase
