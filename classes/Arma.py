8da46b21f37d10e4fc44800397b29b268aec19f4
blesstherodrigo <rodrigo_ade@icloud.com>
2026-05-06 22:01:08 -0300

    resetar classes

--- classes/Arma.py
+++
@@ -1,4 +0,0 @@
-class Arma:
-    def __init__(self, nome, dano):
-        self.nome = nome
-        self.dano = dano
--- classes/jogador.py
+++
@@ -1,7 +0,0 @@
-class Personagem:
-    def __init__(self, nome, vida, defesa, velocidade, danobase):
-        self.nome = nome
-        self.vida = vida
-        self.defesa = defesa
-        self.velocidade  = velocidade
-        self.danobase = danobase
