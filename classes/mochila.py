a4f35778230d9b89acd1f4e2df500a2332b74a73
blesstherodrigo <rodrigo_ade@icloud.com>
2026-05-06 22:46:38 -0300

    FEATURE: Adicionando as classes Medicina, Mochila e Munição

---
+++ classes/mochila.py
@@ -0,0 +1,12 @@
+class Mochila():
+    def __init__(self, antidoto, kit_medico, adrenalina, soro, municao1, municao2, municao3, municao4):
+        self.antidoto = antidoto
+        self.kit_medico = kit_medico
+        self.adrenalina = adrenalina
+        self.soro = soro
+        self.municao1 = municao1
+        self.municao2 = municao2
+        self.municao3 = municao3
+        self.municao4 = municao4
+
+    # municao 1, 2, 3, 4 porque ainda não foi definido os nomes de cada tipo de munição
---
+++ classes/municao.py
@@ -0,0 +1,6 @@
+class Municao():
+    def __init__(self, tipo, dano_base, dano_vantajoso, dano_desvantajoso):
+        self.tipo = tipo
+        self.dano_base = dano_base
+        self.dano_vantajoso = dano_vantajoso
+        self.dano_desvantajoso = dano_desvantajoso
