5b5bf69491a5516b2061be48d5ab17e859545119
blesstherodrigo <rodrigo_ade@icloud.com>
2026-05-08 23:21:41 -0300

    Mudança em textos

--- game/exploracao.py
+++ game/exploracao.py
@@ -4,17 +4,13 @@ from objetos.zumbis import zumbis_fase_hospital
 from game.combate import combate
 
 def explorar(jogador):
+
     print("\nVocê começa a explorar...")
 
     print(f"Um {zumbis_fase_hospital[0].nome} apareceu!")
-    resultado = combate(jogador, zumbis_fase_hospital[0])
+    resultado_do_combate = combate(jogador, zumbis_fase_hospital[0])
 
-    if resultado == "morreu":
+    if resultado_do_combate == "morreu":
         return "morreu"
 
-    print("O lugar ficou silencioso...")
-    print("Apareceu outro zumbi.")
-
-    resultado = combate(jogador, zumbis_fase_hospital[1])
-
-    return resultado
+    return resultado_do_combate
