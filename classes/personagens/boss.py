d574e36f22f722ce93280df8dbe848cc8ea0d00a
Rodrigo Araújo <rodrigo_ade@icloud.com>
2026-05-08 23:31:03 -0300

    Adiciona ataque personalizado do boss

--- classes/personagens/boss.py
+++ classes/personagens/boss.py
@@ -6,3 +6,7 @@ from classes.personagens.personagem import Personagem
 class Boss(Personagem):
     def __init__(self, nome, vida, vida_max, dano):
         super().__init__(nome, vida, vida_max, dano)
+
+    def atacar(self, alvo):
+        super().atacar(alvo)
+        print(f"{self.nome} desferiu um ataque pesado e causou {self.dano} de dano")
