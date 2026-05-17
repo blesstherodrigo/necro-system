f548898fbf9c9a219b11a69f4abe5b8df4336d02
blesstherodrigo <rodrigo_ade@icloud.com>
2026-05-17 16:42:02 -0300

    FEATURE: classe Fase

---
+++ classes/fase.py
@@ -0,0 +1,7 @@
+class Fase:
+    def __init__(self, numero, nome, descricao, recompensa, inimigos):
+        self.numero = numero
+        self.nome = nome
+        self.descricao = descricao
+        self.recompensa = recompensa
+        self.inimigos = inimigos
--- classes/itens/municao.py
+++ classes/itens/municao.py
@@ -1,8 +1,8 @@
 # classes/itens/municao.py
 
 class Municao:
-    def __init__(self, tipo, dano_base, dano_vantajoso, dano_desvantajoso):
+    def __init__(self, tipo, dano_base, dano_vantajoso, preco):
         self.tipo = tipo
         self.dano_base = dano_base
         self.dano_vantajoso = dano_vantajoso
-        self.dano_desvantajoso = dano_desvantajoso
+        self.preco = preco
